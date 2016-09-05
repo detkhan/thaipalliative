4eb51a1ddc2ad24908603a1c07ab40ce2d793190
Yaowaluck Promdee <ypromdee@gmail.com>
2016-09-05 09:50:58 +0700

    table6

---
+++ module/thaipalliative/views/report-sec2/table6.php
@@ -0,0 +1,235 @@
+<?
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\helpers\Url;
+
+
+$domain=Url::home();
+?>
+<div class="container">
+  <h2>ตารางที่  6</h2>
+  <p>จำนวนและร้อยละหน่วยบริการและผู้ป่วย เฉพาะกลุ่มโรค Cancer ทุกประเภท</p>
+  <table class="table table-border">
+    <thead>
+  <tr  bgcolor="#92db1e">
+    <td rowspan="3"><center>ระดับ</center></td>
+    <td colspan="4"><center>ทั้งหมด</center>
+
+  </td>
+   <td colspan="4" ><center>เฉพาะระหว่าง
+<ว/ด/ป> ถึง <ว/ด/ป></center>
+</td>
+  </tr >
+  <tr bgcolor="#92db1e"><td colspan="2"><center>หน่วยบริการ</center>
+  <td colspan="2"><center>ผู้ป่วย</center>
+  <td colspan="2"><center>หน่วยบริการ</center>
+  <td colspan="2"><center>ผู้ป่วย</center>
+  </tr>
+
+  <tr bgcolor="#92db1e">
+    <td><center>จำนวน</center></td>
+    <td><center>ร้อยละ</center></td>
+   <td><center>จำนวน</center></td>
+    <td><center>ร้อยละ</center></td>
+  <td><center>จำนวน</center></td>
+    <td><center>ร้อยละ</center></td>
+   <td><center>จำนวน</center></td>
+    <td><center>ร้อยละ</center></td>
+
+  </tr>
+
+  </thead>
+<tbody>
+  <tr bgcolor="#96db1e">
+    <td >ทั้งหมดใน Palliative Care Cloud</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>เขตสุขภาพที่ x</td>
+    <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>เขตสุขภาพที่ x</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  <tr >
+    <td >เขตสุขภาพที่ x</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>เขตสุขภาพที่ x</td>
+    <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor "#84db1e">รวม</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  <tr bgcolor="#96db1e">
+    <td >เขตสุขภาพที่เลือกแสดงรายงาน (เขต x)</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>จังหวัด x</td>
+    <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>จังหวัด x</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+<tr>
+    <td>จังหวัด x</td>
+    <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>จังหวัด x</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+<tr bgcolor="#96db1e">
+    <td >หน่วยบริการที่เลือกแสดงรายงาน (ถ้ามี)</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+   <td align='right'>&nbsp;</td>
+  </tr>
+  </tbody>
+  <tfoot>
+  <tr bgcolor="#92db1e">
+    <td align="right"></td>
+    <td>..</td>
+    <td>..</td>
+    <td>..</td>
+
+  <td>..</td>
+   <td>..</td>
+    <td>..</td>
+    <td>..</td>
+
+  <td>..</td>
+  </tr>
+  </tfoot>
+</table></div>
+  <?php
+$totacount=count($dataArrayTable1);
+for ($i=0; $i < $totacount; $i++)
+  {
+   ?>
+  <tr>
+
+     <td align='right'>จังหวัด</td>
+     <td align='right'>จำนวน</td>
+     <td align='right'>ร้อยละ</td>
+	 <td align='right'>จำนวน</td>
+	 <td align='right'>ร้อยละ</td>
+  </tr>
+  <?php
+}
+   ?>
+
+   </tbody>
+  <tfoot>
+    <tr>
+      <td bgcolor "#84db1e">รวม</td>
+       <td align='right'>&nbsp;</td>
+       <td align='right'>&nbsp;</td>
+       <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+       <td align='right'>&nbsp;</td>
+       <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+    </tr>
+    </tfoot>
+    <tr bgcolor="#96db1e">
+      <td >เขตสุขภาพที่เลือกแสดงรายงาน (เขต x)</td>
+       <td align='right'>&nbsp;</td>
+       <td align='right'>&nbsp;</td>
+       <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+       <td align='right'>&nbsp;</td>
+       <td align='right'>&nbsp;</td>
+     <td align='right'>&nbsp;</td>
+    </tr>
+</table>
+</div>
