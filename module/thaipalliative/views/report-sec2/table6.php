a5667408151382ac62293dcc708c5a53ad03518c
Yaowaluck Promdee <ypromdee@gmail.com>
2016-09-05 14:25:15 +0700

    table30

--- module/thaipalliative/views/report-sec2/table6.php
+++ module/thaipalliative/views/report-sec2/table6.php
@@ -9,7 +9,7 @@ $domain=Url::home();
 <div class="container">
   <h2>ตารางที่  6</h2>
   <p>จำนวนและร้อยละหน่วยบริการและผู้ป่วย เฉพาะกลุ่มโรค Cancer ทุกประเภท</p>
-  <table class="table table-hover">
+  <table class="table table-bordered">
     <thead>
   <tr  bgcolor="#88DDBB">
     <td rowspan="3"><center>ระดับ</center></td>
--- module/thaipalliative/views/report-sec3/table30.php
+++ module/thaipalliative/views/report-sec3/table30.php
@@ -11,119 +11,203 @@ $domain=Url::home();
   <p>จำนวนและร้อยละผู้ป่วย จำแนกตามผลการประเมินด้านจิตวิญญาณ เป็นรายโรค ระหว่าง <ว/ด/ป> ถึง <ว/ด/ป></p>
   <table class="table table-bordered">
     <thead>
-  <tr>
-    <td rowspan="2"><center>โรค</center></td>
-    <td colspan="2"><center>ปัญหาเศรษฐกิจ</center></td>
-   <td colspan="2" ><center>ปัญหาจิตใจ</center></td>
-    <td colspan="2" ><center>ปัญหาสังคม</center></td>
-  </tr>
-  <tr>
-    <td><center>จำนวน</center></td>
-    <td><center>ร้อยละ</center></td>
-   <td><center>จำนวน</center></td>
-    <td><center>ร้อยละ</center></td>
-  <td><center>จำนวน</center></td>
-    <td><center>ร้อยละ</center></td>
-  </tr>
-  </thead>
-  <tbody>
-  <tr >
-    <td>Cancer ทุกประเภท</td>
-    <td align='right'></td>
-    <td align='right'></td>
-    <td align='right'></td>
-   <td align='right'></td>
-   <td align='right'></td>
-   <td align='right'></td>
+      <tr>
+        <td rowspan="2"><center>โรค</center></td>
+        <td colspan="2"><center>Hope</center></td>
+       <td colspan="2" ><center>Fear</center></td>
+        <td colspan="2" ><center>Unfinished business</center></td>
+    	 <td colspan="2"><center>Connected
+    ness
+    </center></td>
+       <td colspan="2" ><center>Control helplessness</center></td>
+        <td colspan="2" ><center>Religious</center></td>
+      </tr>
+      <tr>
+        <td><center>n</center></td>
+        <td><center>%</center></td>
+    	 <td><center>n</center></td>
+        <td><center>%</center></td>
+    	 <td><center>n</center></td>
+        <td><center>%</center></td>
+    	 <td><center>n</center></td>
+        <td><center>%</center></td>
+    	 <td><center>n</center></td>
+        <td><center>%</center></td>
+    	 <td><center>n</center></td>
+        <td><center>%</center></td>
+
+      </tr>
+      </thead>
+    <tbody>
+      <tr >
+        <td>Cancer ทุกประเภท</td>
+        <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
 
 
-  </tr>
-  <tr>
-    <td >เฉพาะมะเร็งท่อน้ำดี</td>
-    <td align='right'>&nbsp;</td>
-    <td align='right'>&nbsp;</td>
-     <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
+      </tr>
+      <tr>
+        <td >เฉพาะมะเร็งท่อน้ำดี</td>
+        <td align='right'>&nbsp;</td>
+        <td align='right'>&nbsp;</td>
+         <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
 
-  </tr>
-  <tr>
-    <td >End stage renal diseases</td>
-    <td align='right'>&nbsp;</td>
-    <td align='right'>&nbsp;</td>
-     <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
+      </tr>
+      <tr>
+        <td >End stage renal diseases</td>
+        <td align='right'>&nbsp;</td>
+        <td align='right'>&nbsp;</td>
+         <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
 
-  </tr>
-  <tr>
-    <td >End stage trauma</td>
-    <td align='right'>&nbsp;</td>
-    <td align='right'>&nbsp;</td>
-     <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
+      </tr>
+      <tr>
+        <td >Frailty dementia</td>
+        <td align='right'>&nbsp;</td>
+        <td align='right'>&nbsp;</td>
+         <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
 
-  </tr>
-  <tr>
-    <td >End stage lung diseases</td>
-    <td align='right'>&nbsp;</td>
-    <td align='right'>&nbsp;</td>
-     <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
+      </tr>
+      <tr>
+        <td >End stage lung diseases</td>
+        <td align='right'>&nbsp;</td>
+        <td align='right'>&nbsp;</td>
+         <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
 
-  </tr>
-   <tr>
-    <td >Neurological diseases</td>
-    <td align='right'>&nbsp;</td>
-    <td align='right'>&nbsp;</td>
-     <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
+      </tr>
+       <tr>
+        <td >Neurological diseases</td>
+        <td align='right'>&nbsp;</td>
+        <td align='right'>&nbsp;</td>
+         <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
 
-  </tr>
-   <tr>
-    <td >End stage heart diseases</td>
-    <td align='right'>&nbsp;</td>
-    <td align='right'>&nbsp;</td>
-     <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
+      </tr>
+       <tr>
+        <td >End stage heart diseases</td>
+        <td align='right'>&nbsp;</td>
+        <td align='right'>&nbsp;</td>
+         <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
 
-  </tr>
-  <tr>
-    <td >อื่นๆ .....................................................</td>
-    <td align='right'>&nbsp;</td>
-    <td align='right'>&nbsp;</td>
-     <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
-   <td align='right'>&nbsp;</td>
+      </tr>
+      <tr>
+        <td >อื่นๆ .....................................................</td>
+        <td align='right'>&nbsp;</td>
+        <td align='right'>&nbsp;</td>
+         <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
 
-  </tr>
+      </tr>
+       <tr>
+        <td >อื่นๆ .....................................................</td>
+        <td align='right'>&nbsp;</td>
+        <td align='right'>&nbsp;</td>
+         <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'>&nbsp;</td>
+    	 <td align='right'></td>
+        <td align='right'></td>
+        <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
+    	 <td align='right'></td>
 
+      </tr>
 
-  </tbody>
-  <tfoot>
-  <tr bgcolor="#ecee1c">
-    <td align="right">รวมทั้งหมด</td>
-    <td>..</td>
-    <td>..</td>
-    <td>..</td>
-    <td>..</td>
-    <td>..</td>
-    <td>..</td>
-  </tr>
-  </tfoot>
-  </table></div>
+      </tbody>
+      <tfoot>
+      <tr bgcolor="#ecee1c">
+        <td align="right">รวมทั้งหมด</td>
+    	  <td>..</td>
+        <td>..</td>
+        <td>..</td>
 
+    	<td>..</td>
+    	<td>..</td>
+
+    	<td>..</td>
+    	 <td>..</td>
+        <td>..</td>
+        <td>..</td>
+
+    	<td>..</td>
+    	<td>..</td>
+
+    	<td>..</td>
+      </tr>
+      </tfoot>
+    </table></div>
   <?php
   $totacount=count($dataArrayTable1);
   for ($i=0; $i < $totacount; $i++)
